f0574d52b3cbf74eb735d0219821f12e2f640e55
Chineek Favors <chineekfavors01@gmail.com>
2019-12-20 18:36:40 -0800

    commiting to push changes

--- chef-portfolio3/src/App.js
+++ chef-portfolio3/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import {LoginPage} from './components/LoginPage';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoginPage />
      
     </div>
   );
