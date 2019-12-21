1d4af64d664508ff6a74b0f599bc924924f73bec
Chineek Favors <chineekfavors01@gmail.com>
2019-12-20 18:43:52 -0800

    removed LoginPage from rendering

--- chef-portfolio3/src/App.js
+++ chef-portfolio3/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import {LoginPage} from './components/LoginPage';
+import {CreatepostPage} from './components/CreatepostPage'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+     
      
     </div>
   );
