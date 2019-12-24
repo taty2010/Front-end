f0700b14c37f2661ae3ad5512f2039e3d5bbe78b
taty2010 <shydalys123@gmail.com>
2019-12-23 20:09:01 -0600

    deleted front-end folder and moved files over to shecportfolio folder and updated dependencies and import for components

--- chef-portfolio3/src/App.js
+++ chef-portfolio3/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 import {LoginPage} from './components/LoginPage';
-import {CreatepostPage} from './components/CreatepostPage'
+import {CreatepostPage} from './components/CreatepostPage';
+import Home from "./components/Home/Home";
+import Nav from "./components/Home/Nav/Nav";
+import Register from "../src/components/Register/Register";
+import { Switch, Route, Link } from "react-router-dom";
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
-     
+     <Nav />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Register} />
     </div>
   );
 }
---
+++ chef-portfolio3/src/components/Register/Register.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../img/cheftopia_logo.png";
+import { withFormik, Form, Field } from "formik";
+
+const Register = () => {
+  const Wrap = styled.div`
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+      url("https://images.pexels.com/photos/1640774/pexels-photo-1640774.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260");
+    width: 100%;
+    height: 500px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    padding: 10% 0;
+    margin-top: 12%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
+  const Block = styled.div`
+    background-color: rgba(255, 255, 255, 0.9);
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column;
+    width: 60%;
+    height: auto;
+  `;
+
+  return (
+    <Wrap>
+      <Block>
+        <div>
+          <img src={logo} alt="chef topia logo" width="100" height="100" />
+          <h2>Registration Form</h2>
+        </div>
+        <Form>
+          <Field type="text" name="firstname" placeholder="First Name" />
+          <Field type="text" name="lastname" placeholder="Last Name" />
+          <Field type="email" name="email" placeholder="Email" />
+          <Field type="text" name="username" placeholder="Username" />
+          <Field type="password" name="password" placeholder="Password" />
+          <button className="registerSubmit">Submit</button>
+        </Form>
+      </Block>
+    </Wrap>
+  );
+};
+
+const FormikRegistrationForm = withFormik({
+  toValues(props) {
+    return {
+      firstname: props.firstname || "",
+      lastname: props.lastname || "",
+      email: props.email || "",
+      username: props.username || "",
+      password: props.password || ""
+    };
+  },
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(Register);
+
+export default FormikRegistrationForm;
